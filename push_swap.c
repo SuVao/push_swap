a811933282bcf731bdb6f2736e57f71518dbc208
SuVao <pedro.radon@gmail.com>
2024-06-12 20:42:03 +0100

    s

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:24:42 by pesilva-          #+#    #+#             */
-/*   Updated: 2024/06/12 17:19:16 by pesilva-         ###   ########.fr       */
+/*   Updated: 2024/06/12 20:40:50 by pesilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,14 @@ int main(int ac, char **av)
 {
 	t_stack	*a;
 	t_stack	*b;
-/* 	t_stack	*tmp; */
+	t_stack	*tmp;
 
 	a = NULL;
 	b = NULL;
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (-1);
 	a = stackar_split(av);
-	/*tmp = a;
- 	while (a)
-	{
-		printf("%d\n", a->nbr);
-		a = a->next;
-	} */
+
 	if (is_sorted(a))
 		return (0);
 	else if (stack_size(a) <= 3)
@@ -38,10 +33,13 @@ int main(int ac, char **av)
 		sort_5(a, b);
 /* 	else
 		sort_big(a, b); */
-/* 	while (tmp)
+	tmp = b;
+	while (tmp)
 	{
 		printf("%d\n", tmp->nbr);
 		tmp = tmp->next;
-	} */
+	}
+
+	
 	return (0);
 }
